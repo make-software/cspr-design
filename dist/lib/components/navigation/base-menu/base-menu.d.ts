7c39bcd7d73312bd41ee25cf6c9fad881b421dc9
eugenebelov <evgeniy@make.services>
2023-04-10 16:47:03 +0300

    update lib

---
+++ dist/lib/components/navigation/base-menu/base-menu.d.ts
@@ -0,0 +1,4 @@
+import { PropsWithChildren } from "react";
+import { MenuProps } from "../dropdown-menu/menu/menu";
+export declare const BaseMenu: ({ opened, onClose, children, }: PropsWithChildren<MenuProps>) => JSX.Element;
+//# sourceMappingURL=base-menu.d.ts.map
