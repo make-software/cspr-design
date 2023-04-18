d86a9052e3c6e8010b99bfc2de424a33613dce62
sasha kushnir <sasha@make.sevices>
2023-04-18 19:21:32 +0300

    Changed quotes

--- src/lib/components/avatar/avatar.stories.tsx
+++ src/lib/components/avatar/avatar.stories.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import styled from 'styled-components';
 import Avatar from './avatar'
-import FlexRow from "../flex-row/flex-row";
-import FlexColumn from "../flex-column/flex-column";
+import FlexRow from '../flex-row/flex-row';
+import FlexColumn from '../flex-column/flex-column';
 import SubtitleText from '../subtitle-text/subtitle-text';
 
 export default {
@@ -32,11 +32,11 @@ const AvatarTypes = ({size}) => (
         <SubtitleText size={2}>{size}</SubtitleText>
         <StyledBlock>
             <Avatar
-                hash={'2132460a950062fd30ad8a54cda9d0bed7ee1571edeb65459f62f3562ed82884'}
+                hash='2132460a950062fd30ad8a54cda9d0bed7ee1571edeb65459f62f3562ed82884'
                 size={size}/>
-            <Avatar hash={'fhrse934-3400343!dede@frfrkrmbTFSDEFTH'} size={size}/>
+            <Avatar hash='fhrse934-3400343!dede@frfrkrmbTFSDEFTH' size={size}/>
             <Avatar isErc20 size={size}/>
-            <Avatar src={'https://make.services/logo.svg'} size={size}/>
+            <Avatar src='https://make.services/logo.svg' size={size}/>
         </StyledBlock>
     </>
 );
@@ -44,10 +44,10 @@ const AvatarTypes = ({size}) => (
 export const allOptions = () => (
     <FlexRow itemsSpacing={10}>
         <FlexColumn itemsSpacing={10}>
-            <AvatarTypes size={'small'}/>
-            <AvatarTypes size={'default'}/>
-            <AvatarTypes size={'medium'}/>
-            <AvatarTypes size={'big'}/>
+            <AvatarTypes size='small'/>
+            <AvatarTypes size='default'/>
+            <AvatarTypes size='medium'/>
+            <AvatarTypes size='big'/>
         </FlexColumn>
     </FlexRow>
 );
