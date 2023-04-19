352159da1e0ee017b7c95c38cf1cc29a77d46294
sasha kushnir <sasha@make.sevices>
2023-04-19 11:32:53 +0300

    Added storybook for BodyText, CaptionText, HeaderText and Link components

---
+++ src/lib/components/header-text/header-text.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import HeaderText from './header-text';
+import FlexRow from '../flex-row/flex-row';
+import FlexColumn from '../flex-column/flex-column';
+import BodyText from '../body-text/body-text';
+import TruncateBox from '../truncate-box/truncate-box';
+
+export default {
+    component: HeaderText,
+    title: 'HeaderText',
+    args: {
+        size: 1,
+        variation: 'darkGray',
+        monotype: false,
+        uppercase: false,
+        capitalize: false,
+        loading: false,
+    },
+} as ComponentMeta<typeof HeaderText>;
+
+const Template: ComponentStory<typeof HeaderText> = (args) => (
+    <FlexRow itemsSpacing={30}>
+        <FlexColumn itemsSpacing={20}>
+            <BodyText size={1}>Simple Text</BodyText>
+            <HeaderText {...args}>Inter, Bold, 24 pt - Header</HeaderText>
+        </FlexColumn>
+        <FlexColumn itemsSpacing={20}>
+            <BodyText size={1}>Long Text</BodyText>
+            <HeaderText {...args}>
+                <TruncateBox size={1}>
+                    01f5f1fa995ab7e966428e5a1aed797526ad5b2454c50a63a7aaa2dfeae6a996c2
+                </TruncateBox>
+            </HeaderText>
+        </FlexColumn>
+    </FlexRow>
+);
+
+export const Primary = Template.bind({});
---
+++ src/lib/components/link/link.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import Link from './link';
+import FlexRow from '../flex-row/flex-row';
+import FlexColumn from '../flex-column/flex-column';
+import TruncateBox from "../truncate-box/truncate-box";
+import BodyText from "../body-text/body-text";
+
+export default {
+    component: Link,
+    title: 'Link',
+    args: {
+        color: 'primaryBlue',
+        href: './',
+        onClick: () => {},
+    },
+} as ComponentMeta<typeof Link>;
+
+const Template: ComponentStory<typeof Link> = (args) => (
+    <FlexRow itemsSpacing={20}>
+        <FlexColumn itemsSpacing={10}>
+            <BodyText size={2}>Simple Link</BodyText>
+            <Link {...args}>Inner App Link</Link>
+        </FlexColumn>
+        <FlexColumn itemsSpacing={10}>
+            <BodyText size={2}>Long Truncated Link</BodyText>
+            <Link {...args}>
+                <TruncateBox size={1}>
+                    01f5f1fa995ab7e966428e5a1aed797526ad5b2454c50a63a7aaa2dfeae6a996c2
+                </TruncateBox>
+            </Link>
+        </FlexColumn>
+    </FlexRow>
+);
+
+export const Primary = Template.bind({});
