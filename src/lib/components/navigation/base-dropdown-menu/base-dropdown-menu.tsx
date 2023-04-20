407ca53e84be02d6e217a2fc219502f4129bea4a
eugenebelov <evgeniy@make.services>
2023-04-20 18:44:50 +0300

    rewrite base dropdown

--- src/lib/components/navigation/base-dropdown-menu/base-dropdown-menu.tsx
+++ src/lib/components/navigation/base-dropdown-menu/base-dropdown-menu.tsx
@@ -1,23 +1,23 @@
-import React, { PropsWithChildren, useEffect, useState } from "react";
-import { useClickAway } from "../../../hooks/use-click-away";
-import { MenuProps } from "../dropdown-menu/dropdown-menu";
-import styled from "styled-components";
-import FlexRow from "../../flex-row/flex-row";
+import React, { PropsWithChildren, useEffect, useState } from 'react';
+import { useClickAway } from '../../../hooks/use-click-away';
+import { MenuProps } from '../dropdown-menu/dropdown-menu';
+import styled from 'styled-components';
+import FlexRow from '../../flex-row/flex-row';
 
 const Container = styled(FlexRow)<{ isOpen: boolean }>(({ theme, isOpen }) => ({
-    borderRadius: theme.borderRadius.base,
-    ...(!isOpen && {
-        display: "none",
-    }),
+  borderRadius: theme.borderRadius.base,
+  ...(!isOpen && {
+    display: 'none',
+  }),
 }));
 
 const BaseMenuWrapper = styled.div(({ theme }) => ({
-    width: "fit-content",
-    height: "auto",
-    background: theme.styleguideColors.backgroundPrimary,
-    filter: theme.boxShadow.dropdown,
-    borderTop: `5px solid ${theme.styleguideColors.fillPrimaryRed}`,
-    borderRadius: theme.borderRadius.base,
+  width: 'fit-content',
+  height: 'auto',
+  background: theme.styleguideColors.backgroundPrimary,
+  filter: theme.boxShadow.dropdown,
+  borderTop: `5px solid ${theme.styleguideColors.fillPrimaryRed}`,
+  borderRadius: theme.borderRadius.base,
 }));
 
 export const BaseDropdownMenu = ({
@@ -25,22 +25,22 @@ export const BaseDropdownMenu = ({
   onClose,
   children,
 }: PropsWithChildren<MenuProps>) => {
-  // const [isOpen, setIsOpen] = useState(opened);
-  //
-  // const { ref } = useClickAway({
-  //   callback: () => {
-  //     setIsOpen(false);
-  //     onClose && onClose();
-  //   },
-  // });
-  //
-  // useEffect(() => {
-  //   setIsOpen(opened);
-  // }, [opened]);
+  const [isOpen, setIsOpen] = useState(opened);
+
+  useEffect(() => {
+    setIsOpen(opened);
+  }, [opened]);
+
+  const { ref } = useClickAway({
+    callback: () => {
+      setIsOpen(false);
+      onClose && onClose();
+    },
+  });
 
   return (
-    // <Container isOpen={isOpen} ref={ref}>
-    <Container isOpen={opened}>
+    <Container isOpen={isOpen} ref={ref}>
+    {/*<Container isOpen={opened}>*/}
       <BaseMenuWrapper>{children}</BaseMenuWrapper>
     </Container>
   );
