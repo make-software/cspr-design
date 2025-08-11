257eff718cf9f3bc3ef2a50e889ca651b2b1d973
Oleksandr Myshchyshyn <vbobiby.my@gmail.com>
2025-08-11 18:07:10 +0300

    Revert changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,12 +2,12 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  //addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-styling", "@react-theming/storybook-addon", "@storybook/addon-mdx-gfm"],
   addons: ['@storybook/addon-docs', '@storybook/addon-themes'],
-  staticDirs: ['../src/lib/assets', { from: '../public', to: '/app-public' }],
-  framework: { name: '@storybook/react-vite', options: {} },
-  viteFinal: async (viteConfig) => {
-    viteConfig.base = '/';
-    return viteConfig;
+  staticDirs: ['../src/lib/assets', '../public'],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
   },
 };
 export default config;
