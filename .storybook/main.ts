2c474b6a95e63312aea4d9e4661db4ec69e14618
Oleksandr Myshchyshyn <vbobiby.my@gmail.com>
2025-08-11 17:04:21 +0300

    fix build issue

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,18 +1,14 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  //addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-styling", "@react-theming/storybook-addon", "@storybook/addon-mdx-gfm"],
-  "addons": ["@storybook/addon-docs", "@storybook/addon-themes"],
-  "staticDirs": [
-    "../src/lib/assets", "../public"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-themes'],
+  staticDirs: ['../src/lib/assets', '../public'],
+  framework: { name: '@storybook/react-vite', options: {} },
+
+  viteFinal: async (viteConfig) => {
+    viteConfig.base = '/';
+    return viteConfig;
+  },
 };
 export default config;
