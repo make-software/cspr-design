5bdf25e707d089994adf351865a23dce118afff7
Evgeniy Bilov <eugenebelov@users.noreply.github.com>
2025-09-28 23:15:15 +0300

    correct export of address

---
+++ dist/lib/components/pagination/pagination-container.d.ts
@@ -0,0 +1,2 @@
+export declare const PaginationContainer: import('styled-components').StyledComponent<"div", import('styled-components').DefaultTheme, {}, never>;
+//# sourceMappingURL=pagination-container.d.ts.map
